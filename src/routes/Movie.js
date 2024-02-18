c0b35740a7ae16cdeb2ec4308fffa8eb66c3db55
wlarkspur <newacon@naver.com>
2024-02-18 22:52:35 +0200

    Apollo

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,30 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+      small_cover_image
+    }
+  }
+`;
+
 export default function Movie() {
-  return <div>This is movie details</div>;
+  const { id } = useParams();
+  const { data, loading, error } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+  });
+  console.log(id, data, loading);
+  if (loading) {
+    return <h1>Fetching movie...</h1>;
+  }
+  if (error) {
+    return <h1>Something is wrong...check graphql</h1>;
+  }
+  return <div>{data && data.movie.title}</div>;
 }
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,32 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      title
+      id
+    }
+  }
+`;
+
 export default function Movies() {
-  return <div>This is a list of movies.</div>;
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Could not fetch:(</h1>;
+  }
+  return (
+    <div>
+      <h1>Movies</h1>
+      {data.allMovies.map((movie) => (
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
