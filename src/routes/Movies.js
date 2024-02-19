3d625a8f6519cb2deee9387ab766f2e5dc3d16d9
wlarkspur <newacon@naver.com>
2024-02-19 20:38:35 +0200

    apollo gql movie web app

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,32 +1,97 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const ALL_MOVIES = gql`
   query getMovies {
     allMovies {
       title
       id
+      medium_cover_image
     }
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const MoviesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 35px;
+  width: 60%;
+  min-width: 650px;
+  position: relative;
+  top: -50px;
+  justify-content: center;
+`;
+
+const PosterContainer = styled.div`
+  height: 360px;
+  min-height: 250px;
+  border-radius: 7px;
+  width: 110%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  background-color: transparent;
+`;
+
+const PosterBg = styled.div`
+  background-image: url(${(props) => props.background});
+  height: 100%;
+  width: 100%;
+  min-width: 150px;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
+
 export default function Movies() {
-  const { data, loading, error } = useQuery(ALL_MOVIES);
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
-  if (error) {
-    return <h1>Could not fetch:(</h1>;
-  }
+  const { data, loading } = useQuery(ALL_MOVIES);
+
   return (
-    <div>
-      <h1>Movies</h1>
-      {data.allMovies.map((movie) => (
-        <li key={movie.id}>
-          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
-      ))}
-    </div>
+    <Container>
+      <Header>
+        <Title>Apollo Movies</Title>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      <MoviesGrid>
+        {data?.allMovies?.map((movie) => (
+          <PosterContainer key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>
+              <PosterBg background={movie.medium_cover_image} />
+            </Link>
+          </PosterContainer>
+        ))}
+      </MoviesGrid>
+    </Container>
   );
 }
